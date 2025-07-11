fe72f63ad1f2ac465333b4eb070fff29f2c279c3
T Govardhan <govardhant244@gmail.com>
2025-07-11 15:05:11 +0530

    Link updated

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -11,7 +11,7 @@ function RegisterForm({ onRegister }) {
     setErr("");
     setSuccess("");
     try {
-      const res = await fetch("https://expense-tracker-backend-oi0v.onrender.com/expense/register", {
+      const res = await fetch("https://expense-tracker-backend-oi0v.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
